446d22faa5befe3e8bc5c7e6c7d36075c2958e65
marionasef100 <mario100noca@gmail.com>
2024-06-23 04:35:30 +0300

    fetch customer wanted item

--- src/modules/navigation/nav.controller.js
+++ src/modules/navigation/nav.controller.js
@@ -12,8 +12,28 @@ export const getuserPos=async(req,res,next)=>{
     }
     const indX=currentPos.indexX
     const indY=currentPos.indexY
-
-    res.status(200).json({ message: 'Done', })
     console.log(indX,indY);
 
+    res.status(200).json({ message: 'Done', indX,indY })
+
+}
+
+
+//get item position distination
+export const getdestination=async(req ,res,next)=>{
+    const { _barcode  } = req.body;
+  
+  
+    const productt = await productModel.findOne({barcode:_barcode});
+  
+    if (!productt) {
+      return next(
+        new Error("invalid product please check the qunatity", { cause: 400 })
+      );
+    }
+    const destX=productt.indexX
+    const destY=productt.indexY
+       
+    res.status(200).json({ message: 'your destination is ',destX,destY })
+      
 }
--- src/modules/navigation/nav.router.js
+++ src/modules/navigation/nav.router.js
@@ -4,4 +4,5 @@ import * as nC from '..//navigation/nav.controller.js';
 import { asyncHandler } from '../../utils/errorhandling.js'
 
 router.get('/', asyncHandler(nC.getuserPos))
+router.get('/dest',asyncHandler(nC.getdestination))
 export default router
